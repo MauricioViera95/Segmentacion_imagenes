25a501bd45da54714c1cd34a05370a80c5ea0a9c
Mauricio Viera <97111578+MauricioViera95@users.noreply.github.com>
2025-10-02 20:26:39 -0500

    Delete datasets.py

--- datasets.py
+++
@@ -1,9 +0,0 @@
-from torchgeo.datasets import RasterDataset
-
-# Aplicación de clases torchgeo
-class PNOAImages(RasterDataset): # This can be a single large image raster
-    filename_glob = "*.tif"
-
-class SidewalkLabels(RasterDataset): # But the labels must be split into train, val and test directories
-    filename_glob = "*.tif"
-    is_image = False
